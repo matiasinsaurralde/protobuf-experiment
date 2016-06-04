6b9a68ccdcfe75fc1f27ce1eede4c24b0ae38414
Matias Insaurralde <matias@insaurral.de>
2016-06-04 01:21:42 -0400

    Preparing a MQTT client

--- golang/protobuf.go
+++ golang/protobuf.go
@@ -2,15 +2,26 @@ package main
 
 import(
   "log"
+
   "github.com/golang/protobuf/proto"
+  "github.com/yosssi/gmq/mqtt"
+  "github.com/yosssi/gmq/mqtt/client"
+
   "github.com/matiasinsaurralde/protobuf-experiment/proto"
 )
 
+const MqttUrl string = "mqtt://test.mosquitto.org"
+
+/* Initialize the message protobuf */
+
+func SendMessage( text string ) {
+  m := &experiment.Message{
+    Body: proto.String( text),
+  }
+}
+
+/* The main function */
+
 func main() {
   log.Println("main()")
-  m := &experiment.Message{
-    Body: proto.String("Hello world"),
-  }
-
-  log.Println("m", m)
 }
